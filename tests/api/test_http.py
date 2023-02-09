b212ae6c8d90a6c52978f0e0d3dc5953f8db2638
Anastasiia Kovalenko <anastasiia.kovalenko1@gmail.com>
2023-02-09 16:47:40 +0300

    Added a negative test

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -10,9 +10,18 @@ def test_first_request():
 @pytest.mark.http
 def test_second_request():
     r = requests.get('https://api.github.com/users/defunkt')
+    # намагаємось отримати інформацію про користувача "defunkt" на 'GitHub.com'
     body = r.json()
     headers = r.headers
-    
+
     assert body['name'] == 'Chris Wanstrath'
     assert r.status_code == 200
     assert headers['Server'] == 'GitHub.com'
+
+
+@pytest.mark.http
+def test_status_code_request():
+    r = requests.get('https://api.github.com/users/anstasiia_kovalenko')
+    # намагаємось отримати інформацію про неіснуючого користувача "anstasiia_kovalenko" на 'GitHub.com'
+    # assert r.status_code == 200 (Negative test = failed) тест не пройшов
+    assert r.status_code == 404 # Negative test = пройшов (такого користувача не знайдено)
