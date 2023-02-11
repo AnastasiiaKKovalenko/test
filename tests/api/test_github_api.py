2e251695075dfda407b2ec0f71509463b59c4216
Anastasiia Kovalenko <anastasiia.kovalenko1@gmail.com>
2023-02-11 23:14:38 +0300

    Improved the code

--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -1,18 +1,14 @@
 import pytest
-from modules.api.clients.github import GitHub
 
 
 @pytest.mark.api
-def test_user_exists():
-    api = GitHub()
-    user = api.get_user_defunkt()
-    
+def test_user_exists(github_api):
+    user = github_api.get_user('defunkt')
     assert user['login'] == 'defunkt'
 
-@pytest.mark.api
-def test_user_non_exist():
-    api = GitHub()
-    r = api.get_non_exist_user()
-    # print(r) спочатку вивели директиву в термінал щоб отрімати message для статусу 404, потім взяли змінну 'message' для перевірки
 
+@pytest.mark.api
+def test_user_non_exist(github_api):
+    r = github_api.get_user('butenkosergii')
+    # print(r) спочатку вивели директиву в термінал щоб отрімати message для статусу 404, потім взяли змінну 'message' для перевірки
     assert r['message'] == 'Not Found'
