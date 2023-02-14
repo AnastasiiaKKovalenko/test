3285197718ffc78ccc2fbc2574aaf23a92212e82
Anastasiia Kovalenko <anastasiia.kovalenko1@gmail.com>
2023-02-14 21:35:25 +0300

    wrote my own client that used the GitHub API and added tests for it

--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -12,3 +12,22 @@ def test_user_non_exist(github_api):
     r = github_api.get_user('butenkosergii')
     # print(r) спочатку вивели директиву в термінал щоб отрімати message для статусу 404, потім взяли змінну 'message' для перевірки
     assert r['message'] == 'Not Found'
+
+
+@pytest.mark.api
+def test_repo_can_be_found(github_api):
+    r = github_api.search_repo('become-qa-auto')
+    assert r['total_count'] == 31
+    assert 'become-qa-auto' in r['items'][0]['name']
+
+
+@pytest.mark.api
+def test_repo_cannot_be_found(github_api):
+    r = github_api.search_repo('sergiibutenko_repo_non_exist')
+    assert r['total_count'] == 0
+
+
+@pytest.mark.api
+def test_repo_with_single_char_be_found(github_api):
+    r = github_api.search_repo('s')
+    assert r['total_count'] != 0
